92be8aeb118843c2bde26ebeb234b23b0f00a3cb
DavidSilva32 <david.silvaxd32@gmail.com>
2023-04-04 21:30:18 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
+import {v4 as uuidv4} from 'uuid'
 
 import Tasks from './components/Tasks'
 import './App.css'
+import AddTask from './components/AddTask'
 
 const App = () => {
   // let message = 'Hello world!'
@@ -18,10 +20,21 @@ const App = () => {
     },
   ])
 
+  const handleTaskAddition = (taskTitle) => {
+    const newTasks = [...tasks, {
+      title: taskTitle,
+      id: uuidv4(),
+      completed: false, 
+    }]
+
+    setTasks(newTasks)
+  }
+
   return (
     <>
       <div className='container'>
-        <Tasks tasks={tasks}/>
+        <AddTask handleTaskAddition={handleTaskAddition} />
+        <Tasks tasks={tasks} />
       </div>
     </>
   )
